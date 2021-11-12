79d7ce803b89d1bbb7abd7cc902a52bd7e6ba28a
Robby Bennett <robby@scitools.com>
2021-11-11 17:30:39 -0700

    Started figuring out idle api

--- background.js
+++ background.js
@@ -1,5 +1,21 @@
+focusMode = true;
+
 chrome.commands.onCommand.addListener(command => {
 	if (command == 'focusMode') {
-		alert('Starting focus mode');
+		if (focusMode) {
+			alert('Starting focus mode');
+		} else {
+			alert('All done :)');
+		}
+
+		focusMode = ! focusMode;
 	}
 });
+
+function warning() {
+	if (focusMode) {
+		alert('Inactive for 30 seconds');
+	}
+}
+
+chrome.idle.queryState(30, warning);
